207efdea16ff784870145fd53f5d9b6392644ce6
ishay1997 <ishaylevy8@gmail.com>
2024-02-10 12:32:40 +0200

    remember the best score of the game

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,15 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        # self.high_score = 0
+        # try:
+        with open('data.txt', 'r') as file:
+                # content = int(file.read())
+                # if content:
+            self.high_score = int(file.read())
+        # except FileNotFoundError:
+        #     pass
+
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -18,9 +26,13 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} best score:{self.high_score} ", align=ALIGNMENT, font=FONT)
+
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                self.high_score = self.score
+                file.write(f"{self.high_score}")
+
         self.score = 0
         self.update_scoreboard()
 
